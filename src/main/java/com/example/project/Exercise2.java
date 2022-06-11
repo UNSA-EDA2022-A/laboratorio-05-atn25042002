3d16ebc95ce9eadc4c41ae5c2e559d5bf2608ca2
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-06-10 19:13:09 -0500

    Codigo base - Primer intento

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -15,7 +15,22 @@ public class Exercise2 {
     public boolean existenDuplicados(String str) {
         MyStack<Character> stack = new LinkedListStack<>();
         // Colocar codigo aqui
-
-        return false;
+        String letra= "", abertura= "{([", cierre ="})]", equiv;
+        for(int i= 0; i< s.length(); i++){ // Recorre letra por letra
+            letra= String.valueOf(s.charAt(i)); // Extrae la letra
+            if(abertura.indexOf(letra)>= 0){ // Verifica si es un simbolo de abertura
+                stack.push(letra.charAt(0)); // Lo agrega a la pila
+            }
+            else if(cierre.indexOf(letra)>= 0){ // verifica si es simbolo de cierre
+                equiv= String.valueOf(abertura.charAt(cierre.indexOf(letra))); // Obtiene el simbolo de abertura equivalente
+                if(equiv.equals(String.valueOf(stack.top()))){ // Verifique que el ultimo simbolo de abertura corresponda con el ultimo de cierre ingresado
+                    stack.pop(); //Si coincide lo retira
+                }
+                else{
+                    return true; //Si no coincide es porque es porque no esta balanceado
+                }
+            }
+        }
+        return !stack.isEmpty(); // Verifica que este vacia la lista despues del recorrido
     }
 }
