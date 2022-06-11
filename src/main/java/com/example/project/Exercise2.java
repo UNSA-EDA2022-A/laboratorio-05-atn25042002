5dea6ad2c676ca24f6a5db5c33ebfdfd3a4b8a89
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-06-10 20:09:45 -0500

    corrigiendo funcionamiento

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -16,24 +16,31 @@ public class Exercise2 {
         MyStack<Character> stack = new LinkedListStack<>();
         // Colocar codigo aqui
         String letra= "";
-        int aux = 0;
+        int cont= 0;
         for(int i= 0; i< str.length(); i++){ // Recorre letra por letra
             letra= String.valueOf(str.charAt(i)); // Extrae la letra
             if(letra.equals("(")){ // Verifica si es un simbolo de abertura
+                cont++
                 stack.push(letra.charAt(0)); // Lo agrega a la pila
             }
-            else if(letra.equals(")") && aux> 0){ // verifica si es simbolo de cierre
-                if(")".equals(String.valueOf(stack.top()))){ // Verifique que el ultimo simbolo de abertura corresponda con el ultimo de cierre ingresado
+            else if(letra.equals(")")){ // verifica si es simbolo de cierre
+                if("x".equals(String.valueOf(stack.top())) && cont > 0){ // Verifique que el ultimo simbolo de abertura corresponda con el ultimo de cierre ingresado
                     stack.pop(); //Si coincide lo retira
-                    aux--;
+                    stack.pop();
+                    cont--;
                 }
                 else{
                     return true; //Si no coincide es porque es porque no esta balanceado
                 }
             }
             else {
-                aux++;   
+                if(!"x".equals(String.valueOf(stack.top()))){
+                    stack.push("x".charAt(0));  
+                }
             }
+        }
+        if(String.valueOf(stack.top()).equals("x")){
+            stack.pop();   
         }
         return !stack.isEmpty(); // Verifica que este vacia la lista despues del recorrido
     }
